f1aec0395905f2927aaa388f30cc23dfa32b41c6
Takahiro Masui <takahiro.masui@luxiar.com>
2026-02-22 10:17:49 +0900

    feat: implement user authentication with OmniAuth and create sessions controller

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,9 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2,
+           ENV['GOOGLE_CLIENT_ID'],
+           ENV['GOOGLE_CLIENT_SECRET']
+end
+
+OmniAuth.config.on_failure = proc { |env|
+  OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+}
